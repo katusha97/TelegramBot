ee9c22e1ef2b3afe52dfbcefc6517fbccfc06f02
Deduchenko Katya <deduchenkoe@gmail.com>
2021-04-27 16:57:28 +0300

    init rest

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.30"
+    kotlin("plugin.serialization") version "1.4.30"
+}
+
+group = "me.katya"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+//    implementation("com.github.pengrad:java-telegram-bot-api:5.1.0")
+    implementation ("org.telegram:telegrambots:5.2.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
+}
+
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
